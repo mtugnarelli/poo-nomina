27b5223e853e90c8f9343850d237ae8ae6a9daab
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2020-03-31 12:55:03 -0300

    Tratamiento polimórfico de Empleados

--- src/nomina/PruebaNomina.java
+++ src/nomina/PruebaNomina.java
@@ -35,4 +35,67 @@ public class PruebaNomina {
 
 		Assert.assertEquals(30_666, nomina.promediarSueldos());
 	}
+	
+	@Test
+	public void agregarUnGerente() {
+		
+		Empleado claudio = new Gerente("Claudio");
+		claudio.cambiarSueldoBruto(60_000);
+		
+		nomina.agregar(claudio);
+		
+		Assert.assertEquals(48_000, nomina.promediarSueldos());
+	}
+	
+	@Test
+	public void agregarUnGerenteConViaticos() {
+		
+		Gerente claudio = new Gerente("Claudio");
+		claudio.cambiarSueldoBruto(100_000);
+		claudio.asignarViaticos(10_000);
+		
+		nomina.agregar(claudio);
+		
+		Assert.assertEquals(90_000, nomina.promediarSueldos());
+	}
+	
+	@Test
+	public void agregarUnEmpleadoConUnGerenteYUnDirector() {
+		
+		Empleado nicolas = new Empleado("Nicolas");
+		nicolas.cambiarSueldoBruto(30_000);
+
+		nomina.agregar(nicolas);
+		
+		Gerente natalia = new Gerente("Natalia");
+		natalia.cambiarSueldoBruto(100_000);
+		natalia.asignarViaticos(10_000);
+		
+		nomina.agregar(natalia);
+		
+		Assert.assertEquals(57_000, nomina.promediarSueldos());
+	}
+	
+	@Test
+	public void promediarSueldosDeUnEmpleadosUnGerenteYUnDirector() {
+		
+		Empleado santiago = new Empleado("Santiago");
+		santiago.cambiarSueldoBruto(30_000);
+		
+		nomina.agregar(santiago);
+		
+		Gerente victoria = new Gerente("Victoria");
+		victoria.cambiarSueldoBruto(35_000);
+		victoria.asignarViaticos(5_000);
+		
+		nomina.agregar(victoria);
+
+		Director natalia = new Director("Natalia", 0.5);
+		natalia.cambiarSueldoBruto(50_000);
+		natalia.cumplioObjetivos();
+		
+		nomina.agregar(natalia);
+
+		Assert.assertEquals(39_000, nomina.promediarSueldos());
+	}
 }
